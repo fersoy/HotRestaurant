d1ff4b00f21b70c7ea6896cdd3519e3e25b82236
Andrew Bayles <baylesweb@gmail.com>
2019-05-21 19:54:20 -0500

    Add Dummy Reservation Data

--- server.js
+++ server.js
@@ -11,6 +11,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+var reservations = [
+    {
+      id: 1,
+      name: "Fatih",
+      email: "fatih@gmail.com",
+      phone: "713-555-1234"
+    },
+    {
+        id: 2,
+        name: "Andrew",
+        email: "andrew@gmail.com",
+        phone: "713-555-4321"
+    },
+    {
+        id: 3,
+        name: "Patrick",
+        email: "patrick@gmail.com",
+        phone: "713-555-5678"
+    }
+  ];
+
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
